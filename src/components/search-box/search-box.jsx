5b69e7ee7dcc016afa10c4515e22ea0ac50ac59e
Paloma <palomita.c1@gmail.com>
2020-04-21 00:18:25 +0200

    changes backgroung color

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './search-box.css';
 
+
+
 export const SearchBox = ({placeholder,handleChange}) => (
+   
     <input 
      className='search'
      type='search'
      placeholder={placeholder} 
      onChange={handleChange}
      />
-
+    
 )
