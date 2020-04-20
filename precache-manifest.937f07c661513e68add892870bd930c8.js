3720a1d2ea7a494f05338a7867180370269b4468
Paloma <palomita.c1@gmail.com>
2020-04-20 22:01:01 +0200

    Updates

---
+++ precache-manifest.937f07c661513e68add892870bd930c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a176d00b0ddd2695721c4b434fa352a7",
+    "url": "/top-rated-movies/index.html"
+  },
+  {
+    "revision": "14671baecb8da761c644",
+    "url": "/top-rated-movies/static/css/main.9d76d24a.chunk.css"
+  },
+  {
+    "revision": "3af6ff83bd6c1193fa36",
+    "url": "/top-rated-movies/static/js/2.c8855d3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/top-rated-movies/static/js/2.c8855d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14671baecb8da761c644",
+    "url": "/top-rated-movies/static/js/main.4866949a.chunk.js"
+  },
+  {
+    "revision": "a26e5adf511b26a4220c",
+    "url": "/top-rated-movies/static/js/runtime-main.6c495367.js"
+  }
+]);
